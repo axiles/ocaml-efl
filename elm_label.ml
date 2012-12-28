fef740d2f93a1eff3bdaebf8be66831c86fbfb25
axiles <alexis.bernadet@noos.fr>
2012-12-28 21:00:40 +0100

    Added elm_label_line_wrap_get.

--- elm_label.ml
+++ elm_label.ml
@@ -3,3 +3,6 @@ external add : Evas.obj -> Evas.obj = "ml_elm_label_add"
 external line_wrap_set : Evas.obj -> Elm.wrap_type -> unit =
   "ml_elm_label_line_wrap_set"
 
+external line_wrap_get : Evas.obj -> Elm.wrap_type =
+  "ml_elm_label_line_wrap_get"
+
--- elm_label.mli
+++ elm_label.mli
@@ -2,3 +2,5 @@ val add : Evas.obj -> Evas.obj
 
 val line_wrap_set : Evas.obj -> Elm.wrap_type -> unit
 
+val line_wrap_get : Evas.obj -> Elm.wrap_type
+
