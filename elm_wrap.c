fdf26fa03c21d11e9f9ef79539761cd94b48bfdb
axiles <alexis.bernadet@noos.fr>
2013-01-06 11:13:12 +0100

    Added elm_check_add.

--- elm_wrap.c
+++ elm_wrap.c
@@ -625,3 +625,10 @@ PREFIX value ml_elm_object_focus_set(value v_obj, value v_flag)
         return Val_unit;
 }
 
+PREFIX value ml_elm_check_add(value v_parent)
+{
+        Evas_Object* obj = elm_check_add((Evas_Object*) v_parent);
+        if(obj == NULL) caml_failwith("elm_check_add");
+        return (value) obj;
+}
+
