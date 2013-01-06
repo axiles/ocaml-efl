f296af1e6a2cb652507b96dc46be7e40a8a4b66a
axiles <alexis.bernadet@noos.fr>
2013-01-06 15:39:42 +0100

    Autosave support in test7.ml.

--- test7.ml
+++ test7.ml
@@ -22,11 +22,11 @@ let () =
   Elm_box.pack_end box tb;
   Evas_object.show tb;
 
-  let o = Elm_check.add win in
-  Elm_check.state_set o false;
-  Elm_object.text_set o "Autosave";
-  Elm_box.pack_end tb o;
-  Evas_object.show o;
+  let c = Elm_check.add win in
+  Elm_check.state_set c false;
+  Elm_object.text_set c "Autosave";
+  Elm_box.pack_end tb c;
+  Evas_object.show c;
 
   let o = Elm_button.add win in
   Elm_object.text_set o "b";
@@ -52,6 +52,12 @@ let () =
   Evas_object.size_hint_align_set en Evas.hint_fill Evas.hint_fill;
   Elm_box.pack_end box en;
   Evas_object.show en;
+
+  let autosave_change_cb obj _ =
+    let state = Elm_check.state_get obj in
+    Elm_entry.autosave_set en state;
+    if state then Elm_entry.file_save en in
+  Evas_object_smart.callback_add c "changed" autosave_change_cb;
 
   Elm_object.focus_set en true;
 
