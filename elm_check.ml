c83b41094178819fccd48bfea10df1396af4e845
axiles <alexis.bernadet@noos.fr>
2013-01-06 11:19:07 +0100

    Added elm_check_state_set.

--- elm_check.ml
+++ elm_check.ml
@@ -1,2 +1,5 @@
 external add : Evas.obj -> Evas.obj = "ml_elm_check_add"
 
+external state_set : Evas.obj -> bool -> unit = "ml_elm_check_state_set"
+
+
--- elm_check.mli
+++ elm_check.mli
@@ -1,3 +1,3 @@
 val add : Evas.obj -> Evas.obj
 
-
+val state_set : Evas.obj -> bool -> unit
