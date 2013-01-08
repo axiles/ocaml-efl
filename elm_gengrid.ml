6770885641e4be3a955c953da33aa1b5c1f0826e
axiles <alexis.bernadet@noos.fr>
2013-01-08 18:14:31 +0100

    Added missing field in Elm_Gengrid_Item_Class.

--- elm_gengrid.ml
+++ elm_gengrid.ml
@@ -1,7 +1,8 @@
 type item_class = {
   item_style : string;
   func_text_get : Evas.obj -> string -> string;
-  func_content_get : Evas.obj -> string -> Evas.obj;
+  func_content_get : Evas.obj -> string -> Evas.obj option;
+  func_state_get : Evas.obj -> string -> bool;
   func_content_del : Evas.obj -> unit;
 }
 
