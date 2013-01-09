3f864d7b7f30be0cdf74280005e1ff0351076c74
axiles <alexis.bernadet@noos.fr>
2013-01-09 17:51:40 +0100

    Corrected elm_naviframe_item_simple_push.

--- elm_naviframe.ml
+++ elm_naviframe.ml
@@ -10,7 +10,10 @@ external item_push :
 external item_title_visible_set : Elm_object.item -> bool -> unit =
   "ml_elm_naviframe_item_title_visible_set"
 
-let item_simple_push obj content = item_push obj content ()
+let item_simple_push obj content =
+  let it = item_push obj content () in
+  item_title_visible_set it false;
+  it
 
 external item_simple_promote : Evas.obj -> Evas.obj -> unit =
   "ml_elm_naviframe_item_simple_promote"
