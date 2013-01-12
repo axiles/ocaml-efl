8323e996ac8e52725be959256e9639d3bf22d15b
axiles <alexis.bernadet@noos.fr>
2013-01-12 11:29:40 +0100

    Added elm_radio_wrap.c.

---
+++ elm_radio_wrap.c
@@ -0,0 +1,32 @@
+#include "include.h"
+
+PREFIX value ml_elm_radio_add(value v_parent)
+{
+        Evas_Object* obj = elm_radio_add((Evas_Object*) v_parent);
+        if(obj == NULL) caml_failwith("elm_radio_add");
+        return (value) obj;
+}
+
+PREFIX value ml_elm_radio_state_value_set(value v_obj, value v_v)
+{
+        elm_radio_state_value_set((Evas_Object*) v_obj, Int_val(v_v));
+        return Val_unit;
+}
+
+PREFIX value ml_elm_radio_group_add(value v_obj, value v_group)
+{
+        elm_radio_group_add((Evas_Object*) v_obj, (Evas_Object*) v_group);
+        return Val_unit;
+}
+
+PREFIX value ml_elm_radio_value_get(value v_obj)
+{
+        return Val_int(elm_radio_value_get((Evas_Object*) v_obj));
+}
+
+PREFIX value ml_elm_radio_value_set(value v_obj, value v_v)
+{
+        elm_radio_value_set((Evas_Object*) v_obj, Int_val(v_v));
+        return Val_unit;
+}
+
--- elm_wrap.c
+++ elm_wrap.c
@@ -708,36 +708,6 @@ PREFIX value ml_elm_naviframe_item_simple_promote(value v_obj, value v_content)
         return Val_unit;
 }
 
-PREFIX value ml_elm_radio_add(value v_parent)
-{
-        Evas_Object* obj = elm_radio_add((Evas_Object*) v_parent);
-        if(obj == NULL) caml_failwith("elm_radio_add");
-        return (value) obj;
-}
-
-PREFIX value ml_elm_radio_state_value_set(value v_obj, value v_v)
-{
-        elm_radio_state_value_set((Evas_Object*) v_obj, Int_val(v_v));
-        return Val_unit;
-}
-
-PREFIX value ml_elm_radio_group_add(value v_obj, value v_group)
-{
-        elm_radio_group_add((Evas_Object*) v_obj, (Evas_Object*) v_group);
-        return Val_unit;
-}
-
-PREFIX value ml_elm_radio_value_get(value v_obj)
-{
-        return Val_int(elm_radio_value_get((Evas_Object*) v_obj));
-}
-
-PREFIX value ml_elm_radio_value_set(value v_obj, value v_v)
-{
-        elm_radio_value_set((Evas_Object*) v_obj, Int_val(v_v));
-        return Val_unit;
-}
-
 PREFIX value ml_elm_naviframe_item_title_visible_set(value v_it, value v_flag)
 {
         elm_naviframe_item_title_visible_set((Elm_Object_Item*) v_it,
