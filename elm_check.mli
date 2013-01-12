0ee7480422796ebfc2bdab34d2e4d6daca20ed9b
axiles <alexis.bernadet@noos.fr>
2013-01-12 14:30:37 +0100

    Added some documentation.

--- elm_check.mli
+++ elm_check.mli
@@ -1,3 +1,11 @@
+(** elm_check_* *)
+
+(** The following function will not be interfaced
+
+elm_check_state_pointer_set
+
+Use a callback with signal "changed" instead.*)
+
 val add : Evas.obj -> Evas.obj
 
 val state_set : Evas.obj -> bool -> unit
