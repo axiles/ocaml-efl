c119ee6dc8159196966e2b89b049917e9df49df8
axiles <alexis.bernadet@noos.fr>
2013-01-12 15:15:34 +0100

    Finished test9.ml.

--- test9.ml
+++ test9.ml
@@ -1,6 +1,12 @@
 open Efl
+open Format
+
+let print obj _ =
+  printf "check %smarked\n%!" (if Elm_check.state_get obj then "" else "un")
 
 let () =
+  Elm.init Sys.argv;
+
   let win = Elm_win.add "check" `basic in
   Elm_win.title_set win "check";
   Elm_win.autodel_set win true;
@@ -13,6 +19,7 @@ let () =
 
   let cb = Elm_check.add win in
   Elm_object.text_set cb "checkbox";
+  Evas_object_smart.callback_add cb "changed" print;
   Evas_object.move cb 10 10;
   Evas_object.resize cb 200 30;
   Evas_object.show cb;
@@ -23,6 +30,7 @@ let () =
   Evas_object.show icon;
 
   let cb2 = Elm_check.add win in
+  Elm_object.text_set cb2 "another checkbox";
   Elm_check.state_set cb true;
   Elm_object.part_content_set cb2 ~p:"icon" icon;
   Evas_object.move cb2 10 50;
