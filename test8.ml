0c7c79f77e669f897b0ec93dbb607298e9807a28
axiles <alexis.bernadet@noos.fr>
2013-01-12 13:13:36 +0100

    Done some factorization.

--- test8.ml
+++ test8.ml
@@ -3,6 +3,41 @@ open Efl
 
 let cb obj _ = printf "val is now: %d\n%!" (Elm_radio.value_get obj)
 
+let format_obj obj =
+  Evas_object.size_hint_weight_set obj Evas.hint_expand Evas.hint_expand;
+  Evas_object.size_hint_align_set obj Evas.hint_fill Evas.hint_fill;
+  Evas_object.show obj
+
+type group =
+| Nothing
+| Cb_only
+| Group_and_cb of Evas.obj
+
+let add_radio win bx i icon_name group = 
+  let radio = Elm_radio.add win in
+  Elm_object.text_set radio (sprintf "Radio %d" i);
+  Elm_radio.state_value_set radio i;
+  (match icon_name with
+  | None -> ()
+  | Some s ->
+    let ic = Elm_icon.add win in
+    Elm_icon.standard_set ic s;
+    Elm_object.part_content_set radio ~p:"icon" ic);
+  (match group with
+  | Nothing | Cb_only -> ()
+  | Group_and_cb g -> Elm_radio.group_add radio g);
+  Elm_box.pack_end bx radio;
+  format_obj radio;
+  (match group with
+  | Nothing -> ()
+  | Cb_only | Group_and_cb _ ->
+    Evas_object_smart.callback_add radio "changed" cb);
+  radio
+
+let add_radio_next win bx i icon_name group =
+  let g = match group with None -> Nothing | Some x -> Group_and_cb x in
+  let _ = add_radio win bx i icon_name g in ()
+
 let () =
   Elm.init Sys.argv;
 
@@ -17,56 +52,12 @@ let () =
 
   let bx = Elm_box.add win in
   Elm_box.horizontal_set bx true;
-  Evas_object.size_hint_weight_set bx Evas.hint_expand Evas.hint_expand;
-  Evas_object.size_hint_align_set bx Evas.hint_fill Evas.hint_fill;
+  format_obj bx;
   Elm_win.resize_object_add win bx;
-  Evas_object.show bx;
 
-  let radio = Elm_radio.add win in
-  let group = radio in
-  Elm_object.text_set radio "Radio 1";
-  Elm_radio.state_value_set radio 1;
-  Elm_radio.value_set group 1;
-  let ic = Elm_icon.add win in
-  Elm_icon.standard_set ic "home";
-  Elm_object.part_content_set radio ~p:"icon" ic;
-  Elm_radio.group_add radio group;
-  Elm_box.pack_end bx radio;
-  Evas_object.size_hint_weight_set radio Evas.hint_expand Evas.hint_expand;
-  Evas_object.size_hint_align_set radio Evas.hint_fill Evas.hint_fill;
-  Evas_object.show radio;
-  Evas_object_smart.callback_add radio "changed" cb;
-
-  let radio = Elm_radio.add win in
-  Elm_object.text_set radio "Radio 2";
-  Elm_radio.state_value_set radio 2;
-  let ic = Elm_icon.add win in
-  Elm_icon.standard_set ic "file";
-  Elm_object.part_content_set radio ~p:"icon" ic;
-  Elm_radio.group_add radio group;
-  Elm_box.pack_end bx radio;
-  Evas_object.size_hint_weight_set radio Evas.hint_expand Evas.hint_expand;
-  Evas_object.size_hint_align_set radio Evas.hint_fill Evas.hint_fill;
-  Evas_object.show radio;
-  Evas_object_smart.callback_add radio "changed" cb;
-
-  let radio = Elm_radio.add win in
-  Elm_object.text_set radio "Radio 3";
-  Elm_radio.state_value_set radio 3;
-  Elm_radio.group_add radio group;
-  Elm_box.pack_end bx radio;
-  Evas_object.size_hint_weight_set radio Evas.hint_expand Evas.hint_expand;
-  Evas_object.size_hint_align_set radio Evas.hint_fill Evas.hint_fill;
-  Evas_object.show radio;
-  Evas_object_smart.callback_add radio "changed" cb;
-
-  let radio = Elm_radio.add win in
-  Elm_object.text_set radio "Radio 4";
-  Elm_radio.state_value_set radio 4;
-  Elm_box.pack_end bx radio;
-  Evas_object.size_hint_weight_set radio Evas.hint_expand Evas.hint_expand;
-  Evas_object.size_hint_align_set radio Evas.hint_fill Evas.hint_fill;
-  Evas_object.show radio;
+  let group = Some (add_radio win bx 1 (Some "home") Cb_only) in
+  let list = [(2, Some "file", group); (3, None, group); (4, None, None)] in
+  List.iter (fun (i, icn, g) -> add_radio_next win bx i icn g) list;
 
   Evas_object.show win;
 
