3d6cf8199cf147aac824f1a5d6a3bae2e6c91b41
axiles <alexis.bernadet@noos.fr>
2013-01-12 18:57:31 +0100

    Added file elm_box_wrap.c.

---
+++ elm_box_wrap.c
@@ -0,0 +1,27 @@
+#include "include.h"
+
+PREFIX value ml_elm_box_add(value v_parent)
+{
+        Evas_Object* box = elm_box_add((Evas_Object*) v_parent);
+        if(box == NULL) caml_failwith("elm_box_add");
+        return (value) box;
+}
+
+PREFIX value ml_elm_box_horizontal_set(value v_box, value v_flag)
+{
+        elm_box_horizontal_set((Evas_Object*) v_box, Eina_Bool_val(v_flag));
+        return Val_unit;
+}
+
+PREFIX value ml_elm_box_pack_end(value v_obj, value v_subobj)
+{
+        elm_box_pack_end((Evas_Object*) v_obj, (Evas_Object*) v_subobj);
+        return Val_unit;
+}
+
+PREFIX value ml_elm_box_homogeneous_set(value v_box, value v_flag)
+{
+        elm_box_homogeneous_set((Evas_Object*) v_box, Eina_Bool_val(v_flag));
+        return Val_unit;
+}
+
--- elm_wrap.c
+++ elm_wrap.c
@@ -79,25 +79,6 @@ PREFIX value ml_elm_object_text_set(value v_obj, value v_text)
         return Val_unit;
 }
 
-PREFIX value ml_elm_box_add(value v_parent)
-{
-        Evas_Object* box = elm_box_add((Evas_Object*) v_parent);
-        if(box == NULL) caml_failwith("elm_box_add");
-        return (value) box;
-}
-
-PREFIX value ml_elm_box_horizontal_set(value v_box, value v_flag)
-{
-        elm_box_horizontal_set((Evas_Object*) v_box, Eina_Bool_val(v_flag));
-        return Val_unit;
-}
-
-PREFIX value ml_elm_box_pack_end(value v_obj, value v_subobj)
-{
-        elm_box_pack_end((Evas_Object*) v_obj, (Evas_Object*) v_subobj);
-        return Val_unit;
-}
-
 PREFIX value ml_elm_frame_add(value v_parent)
 {
         Evas_Object* frame = elm_frame_add((Evas_Object*) v_parent);
@@ -114,12 +95,6 @@ PREFIX value ml_elm_object_style_set_with_bool(value v_obj, value v_style)
 PREFIX value ml_elm_object_content_set(value v_obj, value v_content)
 {
         elm_object_content_set((Evas_Object*) v_obj, (Evas_Object*) v_content);
-        return Val_unit;
-}
-
-PREFIX value ml_elm_box_homogeneous_set(value v_box, value v_flag)
-{
-        elm_box_homogeneous_set((Evas_Object*) v_box, Eina_Bool_val(v_flag));
         return Val_unit;
 }
 
