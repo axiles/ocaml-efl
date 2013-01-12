df706c06866a49ead686b7c8191f31b9346589ea
axiles <alexis.bernadet@noos.fr>
2013-01-12 11:33:50 +0100

    Sorted definitions.

--- elm_radio.ml
+++ elm_radio.ml
@@ -1,12 +1,12 @@
 external add : Evas.obj -> Evas.obj = "ml_elm_radio_add"
 
-external state_value_set : Evas.obj -> int -> unit =
-  "ml_elm_radio_state_value_set"
-
 external group_add : Evas.obj -> Evas.obj -> unit =
   "ml_elm_radio_group_add"
 
-external value_get : Evas.obj -> int = "ml_elm_radio_value_get"
+external state_value_set : Evas.obj -> int -> unit =
+  "ml_elm_radio_state_value_set"
 
 external value_set : Evas.obj -> int -> unit = "ml_elm_radio_value_set"
+
+external value_get : Evas.obj -> int = "ml_elm_radio_value_get"
 
--- elm_radio.mli
+++ elm_radio.mli
@@ -1,10 +1,10 @@
 val add : Evas.obj -> Evas.obj
 
-val state_value_set : Evas.obj -> int -> unit
-
 val group_add : Evas.obj -> Evas.obj -> unit
 
-val value_get : Evas.obj -> int
+val state_value_set : Evas.obj -> int -> unit
 
 val value_set : Evas.obj -> int -> unit
+
+val value_get : Evas.obj -> int
 
