1ac9902be85c090d974472a3d04743fb6be0f612
axiles <alexis.bernadet@noos.fr>
2013-01-13 11:09:12 +0100

    Added another possible use of Ecore_Cb.

--- ecore_wrap.c
+++ ecore_wrap.c
@@ -12,3 +12,15 @@ PREFIX void ml_Ecore_Cb_1_free(void* data)
         CAMLreturn0;
 }
 
+PREFIX void ml_Ecore_Cb_free(void* data)
+{
+        CAMLparam0();
+        CAMLlocal1(v_fun);
+        value* v_data = (value*) data;
+        v_fun = *v_data;
+        caml_callback(v_fun, Val_unit);
+        caml_remove_global_root(v_data);
+        free(v_data);
+        CAMLreturn0;
+}
+
--- ecore_wrap.h
+++ ecore_wrap.h
@@ -2,6 +2,7 @@
 #define ECORE_H
 
 PREFIX void ml_Ecore_Cb_1_free(void* data);
+PREFIX void ml_Ecore_Cb_free(void* data);
 
 #endif
 
