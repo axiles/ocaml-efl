f76ea2d180f6ca50ffe26331e7ddc618eb3e9709
axiles <alexis.bernadet@noos.fr>
2013-01-20 19:44:52 +0100

    Added elm_separator_add.

--- elm_separator_wrap.c
+++ elm_separator_wrap.c
@@ -1,2 +1,9 @@
 #include "include.h"
 
+PREFIX value ml_elm_separator_add(value v_parent)
+{
+        Evas_Object* obj = elm_separator_add((Evas_Object*) v_parent);
+        if(obj == NULL) caml_failwith("elm_separator_add");
+        return (value) obj;
+}
+
