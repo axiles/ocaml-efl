30ab717f944c0672e34695dc71d447e3cb089bbd
axiles <alexis.bernadet@noos.fr>
2013-01-20 18:17:50 +0100

    Added elm_image_no_scale_set.

--- elm_image.ml
+++ elm_image.ml
@@ -4,3 +4,5 @@ external file_set : Evas.obj -> string -> ?g:string -> unit -> bool =
 external resizable_set : Evas.obj -> bool -> bool -> unit =
   "ml_elm_image_resizable_set"
 
+external no_scale_set : Evas.obj -> bool -> unit = "ml_elm_image_no_scale_set"
+
--- elm_image.mli
+++ elm_image.mli
@@ -2,3 +2,5 @@ val file_set : Evas.obj -> string -> ?g:string -> unit -> bool
 
 val resizable_set : Evas.obj -> bool -> bool -> unit
 
+val no_scale_set : Evas.obj -> bool -> unit
+
