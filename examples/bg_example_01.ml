4d1d43ff42c1ea12637101f3e8a48cfe9387954a
axiles <alexis.bernadet@noos.fr>
2013-01-23 20:15:26 +0100

    Added example bg_example_01.

---
+++ examples/bg_example_01.ml
@@ -0,0 +1,22 @@
+open Efl
+
+let on_done _ _ = Elm.exit ()
+
+let () =
+  Elm.init Sys.argv;
+
+  let win = Elm_win.add "bg-plain" `basic in
+  Elm_win.title_set win "Bg Plain";
+  Evas_object_smart.callback_add win "delete,request" on_done;
+  Elm_win.autodel_set win true;
+
+  let bg = Elm_bg.add win in
+  Evas_object.size_hint_weight_set bg Evas.hint_expand Evas.hint_expand;
+  Elm_win.resize_object_add win bg;
+  Evas_object.show bg;
+
+  Evas_object.resize win 320 320;
+  Evas_object.show win;
+
+  Elm.run ();
+  Elm.shutdown ()
