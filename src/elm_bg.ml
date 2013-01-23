ef2a21ea67fdfa5919260957624351d574fd6ac4
axiles <alexis.bernadet@noos.fr>
2013-01-23 19:16:55 +0100

    Added elm_bg_file_set.

--- src/elm_bg.ml
+++ src/elm_bg.ml
@@ -1,5 +1,8 @@
 external add : Evas.obj -> Evas.obj = "ml_elm_bg_add"
 
+external file_set : Evas.obj -> string -> ?g:string -> unit -> unit =
+  "ml_elm_bg_file_set"
+
 external color_set : Evas.obj -> int -> int -> int -> unit =
   "ml_elm_bg_color_set"
 
--- src/elm_bg_wrap.c
+++ src/elm_bg_wrap.c
@@ -7,6 +7,16 @@ PREFIX value ml_elm_bg_add(value v_parent)
         return (value) bg;
 }
 
+PREFIX value ml_elm_bg_file_set(
+        value v_obj, value v_file, value v_group, value v_unit)
+{
+        const char* group;
+        if(v_group == Val_int(0)) group = NULL;
+        else group = String_val(Field(v_group, 0));
+        return Val_Eina_Bool(elm_bg_file_set((Evas_Object*) v_obj,
+                String_val(v_file), group));
+}
+        
 PREFIX value ml_elm_bg_color_set(value v_obj, value v_r, value v_g, value v_b)
 {
         elm_bg_color_set((Evas_Object*) v_obj, Int_val(v_r), Int_val(v_g),
