48e6732930767058b39f658fed317085a0fd1191
axiles <alexis.bernadet@noos.fr>
2013-01-25 19:53:22 +0100

    Added example notify_example_01.

---
+++ examples/notify_example_01.ml
@@ -0,0 +1,53 @@
+open Efl
+
+let hide obj _ = Evas_object.hide obj
+
+let add_notify win bx ?orient allow_events ?timeout ?cb name =
+  let content = Elm_label.add win in
+  Elm_object.text_set content name;
+  Evas_object.show content;
+
+  let notify = Elm_notify.add win in
+  (match orient with Some o -> Elm_notify.orient_set notify o | None -> ());
+  Elm_notify.allow_events_set notify allow_events;
+  (match timeout with Some x -> Elm_notify.timeout_set notify x | None -> ());
+  Elm_object.content_set notify content;
+  Evas_object.size_hint_weight_set notify Evas.hint_expand Evas.hint_expand;
+  Evas_object.show notify;
+  Elm_box.pack_end bx notify;
+  (match cb with
+  | Some f-> Evas_object_smart.callback_add notify "block,clicked" f
+  | None -> ())
+
+let add_notify1 win bx = add_notify win bx false ~timeout:5. ~cb:hide "default"
+let add_notify2 win bx name orient = add_notify win bx ~orient true name
+
+let () =
+  Elm.init Sys.argv;
+
+  let win = Elm_win.add "notify" `basic in
+  Elm_win.title_set win "Notify";
+  Elm_win.autodel_set win true;
+  Elm.policy_set `quit `last_window_closed;
+
+  let bg = Elm_bg.add win in
+  Evas_object.size_hint_weight_set bg Evas.hint_expand Evas.hint_expand;
+  Elm_win.resize_object_add win bg;
+  Evas_object.show bg;
+
+  let bx = Elm_box.add win in
+  Elm_box.horizontal_set bx false;
+  Evas_object.size_hint_weight_set bx Evas.hint_expand Evas.hint_expand;
+  Evas_object.size_hint_align_set bx Evas.hint_fill Evas.hint_fill;
+  Elm_win.resize_object_add win bx;
+  Evas_object.show bx;
+  
+  add_notify1 win bx;
+  add_notify2 win bx "bottom" `bottom;
+  add_notify2 win bx "center" `center;
+
+  Evas_object.resize win 100 200;
+  Evas_object.show win;
+
+  Elm.run ();
+  Elm.shutdown ()
