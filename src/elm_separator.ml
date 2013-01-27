270b5cd0c03928fd0192644cffdbcfcaf612f0b1
axiles <alexis.bernadet@noos.fr>
2013-01-27 09:10:39 +0100

    Added src/elm_separator_horizontal_get.

--- src/elm_separator.ml
+++ src/elm_separator.ml
@@ -3,3 +3,5 @@ external add : Evas.obj -> Evas.obj = "ml_elm_separator_add"
 external horizontal_set : Evas.obj -> bool -> unit =
   "ml_elm_separator_horizontal_set"
 
+external horizontal_get : Evas.obj -> bool = "ml_elm_separator_horizontal_get"
+
--- src/elm_separator.mli
+++ src/elm_separator.mli
@@ -2,3 +2,5 @@ val add : Evas.obj -> Evas.obj
 
 val horizontal_set : Evas.obj -> bool -> unit
 
+val horizontal_get : Evas.obj -> bool
+
