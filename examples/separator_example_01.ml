b3aeaf07b59f280b882915f04609b84729c7359b
axiles <alexis.bernadet@noos.fr>
2013-01-27 10:44:03 +0100

    Done some facorization.

--- examples/separator_example_01.ml
+++ examples/separator_example_01.ml
@@ -1,5 +1,15 @@
 open Efl
 
+let add_rect win bx r g b =
+  let rect = Evas_object.rectangle_add (Evas_object.evas_get win) in
+  Evas_object.color_set rect r g b 255;
+  Evas_object.size_hint_min_set rect 90 200;
+  Evas_object.size_hint_weight_set rect Evas.hint_expand Evas.hint_expand;
+  Evas_object.size_hint_align_set rect Evas.hint_fill Evas.hint_fill;
+  Evas_object.show rect;
+  Elm_box.pack_end bx rect
+
+
 let () =
   Elm.init Sys.argv;
 
@@ -19,26 +29,30 @@ let () =
   Elm_win.resize_object_add win bx;
   Evas_object.show bx;
 
-  let rect = Evas_object.rectangle_add (Evas_object.evas_get win) in
+  add_rect win bx 0 255 0;
+
+  (*let rect = Evas_object.rectangle_add (Evas_object.evas_get win) in
   Evas_object.color_set rect 0 255 0 255;
   Evas_object.size_hint_min_set rect 90 200;
   Evas_object.size_hint_weight_set rect Evas.hint_expand Evas.hint_expand;
   Evas_object.size_hint_align_set rect Evas.hint_fill Evas.hint_fill;
   Evas_object.show rect;
-  Elm_box.pack_end bx rect;
+  Elm_box.pack_end bx rect;*)
 
   let separator = Elm_separator.add win in
   Elm_separator.horizontal_set separator true;
   Evas_object.show separator;
   Elm_box.pack_end bx separator;
 
-  let rect = Evas_object.rectangle_add (Evas_object.evas_get win) in
+  add_rect win bx 0 0 255;
+
+  (*let rect = Evas_object.rectangle_add (Evas_object.evas_get win) in
   Evas_object.color_set rect 0 0 255 255;
   Evas_object.size_hint_min_set rect 90 200;
   Evas_object.size_hint_weight_set rect Evas.hint_expand Evas.hint_expand;
   Evas_object.size_hint_align_set rect Evas.hint_fill Evas.hint_fill;
   Evas_object.show rect;
-  Elm_box.pack_end bx rect;
+  Elm_box.pack_end bx rect;*)
 
   Evas_object.show win;
 
