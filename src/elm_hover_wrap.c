385a02fb18829f6d185945032fec119bbda68331
axiles <alexis.bernadet@noos.fr>
2013-01-28 09:36:54 +0100

    Added elm_hover_add.

--- src/elm_hover_wrap.c
+++ src/elm_hover_wrap.c
@@ -1,2 +1,9 @@
 #include "include.h"
 
+PREFIX value ml_elm_hover_add(value v_parent)
+{
+        Evas_Object* obj = elm_hover_add((Evas_Object*) v_parent);
+        if(obj == NULL) caml_failwith("elm_hover_add");
+        return (value) obj;
+}
+
