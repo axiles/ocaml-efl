d30ccd3e4178fdedc4a3c65d9f4fefea9b08b55b
axiles <alexis.bernadet@noos.fr>
2013-01-29 08:26:06 +0100

    Added elm_fileselector_is_save_set.

--- src/elm_fileselector.ml
+++ src/elm_fileselector.ml
@@ -1,2 +1,4 @@
 external add : Evas.obj -> Evas.obj = "ml_elm_fileselector_add"
 
+external is_save_set : Evas.obj -> bool -> unit =
+  "ml_elm_fileselector_is_save_set"
--- src/elm_fileselector.mli
+++ src/elm_fileselector.mli
@@ -1,2 +1,4 @@
 val add : Evas.obj -> Evas.obj
 
+val is_save_set : Evas.obj -> bool -> unit
+
