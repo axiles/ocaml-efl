78f1810703ca03a999c79403a2ee929227c2377d
axiles <alexis.bernadet@noos.fr>
2013-01-30 10:10:19 +0100

    Added safe verof evas_object_smart_callback_add.

--- src/evas_object_smart.ml
+++ src/evas_object_smart.ml
@@ -1,3 +1,6 @@
 external callback_add : Evas.obj -> string -> Evas.smart_cb -> unit =
   "ml_evas_object_smart_callback_add"
 
+let callback_add_safe obj et f =
+  callback_add obj (Evas.Event_type.get_name et) (Evas.Event_type.get_cb et f)
+
--- src/evas_object_smart.mli
+++ src/evas_object_smart.mli
@@ -1,2 +1,4 @@
 val callback_add : Evas.obj -> string -> Evas.smart_cb -> unit
 
+val callback_add_safe : Evas.obj -> ('a, 'b) Evas.Event_type.t -> 'b -> unit
+
