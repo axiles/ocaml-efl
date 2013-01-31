61bafdfd6ca6c695202972a302d75c5d8956acb4
axiles <alexis.bernadet@noos.fr>
2013-01-31 11:24:52 +0100

    Added elm_list_add.

--- src/elm_list_wrap.c
+++ src/elm_list_wrap.c
@@ -1,2 +1,9 @@
 #include "include.h"
 
+PREFIX ml_elm_list_add(value v_parent)
+{
+        Evas_Object* obj = elm_list_add((Evas_Object*) v_parent);
+        if(obj == NULL) caml_failwith("elm_list_add");
+        return (value) obj;
+}
+
