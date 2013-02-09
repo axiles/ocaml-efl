92144b8d159738bae57eed3f3827c044b53e3b38
axiles <alexis.bernadet@noos.fr>
2013-02-09 15:26:46 +0100

    Added elm_naviframe_wrap.c.

--- src/elm_wrap.c
+++ src/elm_wrap.c
@@ -66,13 +66,6 @@ PREFIX value ml_elm_policy_set(value v_policy, value v_value)
                 Elm_Policy_Value_val(v_value)));
 }
 
-PREFIX value ml_elm_naviframe_add(value v_parent)
-{
-        Evas_Object* obj = elm_naviframe_add((Evas_Object*) v_parent);
-        if(obj == NULL) caml_failwith("elm_naviframe_add");
-        return (value) obj;
-}
-
 PREFIX char* ml_Elm_Gen_Item_Text_Get_Cb(
         void* data, Evas_Object* obj, const char* part)
 {
@@ -184,47 +177,6 @@ PREFIX value ml_elm_theme_list_item_path_get(value v_f)
                 free(path);
         }
         CAMLreturn(v);
-}
-
-PREFIX value ml_elm_naviframe_item_push_native(
-        value v_obj, value v_title_label, value v_prev_btn, value v_next_btn,
-        value v_content, value v_item_style, value v_unit)
-{
-        Evas_Object *prev_btn, *next_btn;
-        if(v_prev_btn == Val_int(0)) prev_btn = NULL;
-        else prev_btn = (Evas_Object*) Field(v_prev_btn, 0);
-        if(v_next_btn == Val_int(0)) next_btn = NULL;
-        else next_btn = (Evas_Object*) Field(v_next_btn, 0);
-        char *title_label, *item_style;
-        if(v_title_label == Val_int(0)) title_label = NULL;
-        else title_label = String_val(Field(v_title_label, 0));
-        if(v_item_style == Val_int(0)) item_style = NULL;
-        else item_style = String_val(Field(v_item_style, 0));
-        Elm_Object_Item* item = elm_naviframe_item_push((Evas_Object*) v_obj,
-                title_label, prev_btn, next_btn, (Evas_Object*) v_content,
-                item_style);
-        if(item == NULL) caml_failwith("elm_naviframe_item_push");
-        return (value) item;
-}
-
-PREFIX value ml_elm_naviframe_item_push_byte(value* argv, int argn)
-{
-        return ml_elm_naviframe_item_push_native(argv[0], argv[1], argv[2],
-                argv[3], argv[4], argv[5], argv[6]);
-}
-
-PREFIX value ml_elm_naviframe_item_simple_promote(value v_obj, value v_content)
-{
-        elm_naviframe_item_simple_promote((Evas_Object*) v_obj,
-                (Evas_Object*) v_content);
-        return Val_unit;
-}
-
-PREFIX value ml_elm_naviframe_item_title_visible_set(value v_it, value v_flag)
-{
-        elm_naviframe_item_title_visible_set((Elm_Object_Item*) v_it,
-                Eina_Bool_val(v_flag));
-        return Val_unit;
 }
 
 PREFIX value ml_elm_need_ethumb(value v_unit)
