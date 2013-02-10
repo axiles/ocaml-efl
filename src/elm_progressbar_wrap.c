90ac5514626b9abffeebbc27a49f4ed6e1851382
axiles <alexis.bernadet@noos.fr>
2013-02-10 10:19:23 +0100

    Added elm_progressbar_add.

--- src/elm_progressbar_wrap.c
+++ src/elm_progressbar_wrap.c
@@ -1,2 +1,9 @@
 #include "include.h"
 
+PREFIX value ml_elm_progressbar_add(value v_parent)
+{
+        Evas_Object* obj = elm_progressbar_add((Evas_Object*) v_parent);
+        if(obj == NULL) caml_failwith("elm_progressbar_add");
+        return (value) obj;
+}
+
