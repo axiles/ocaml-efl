d514691b3bacb908349acd21b9fdfa8d1f32c347
axiles <alexis.bernadet@noos.fr>
2013-02-16 15:20:41 +0100

    Added elm_genlist_add.

--- src/elm_genlist_wrap.c
+++ src/elm_genlist_wrap.c
@@ -1,2 +1,9 @@
 #include "include.h"
 
+PREFIX value ml_elm_genlist_add(value v_parent)
+{
+        Evas_Object* obj = elm_genlist_add((Evas_Object*) v_parent);
+        if(obj == NULL) caml_failwith("elm_genlist_add");
+        return (value) obj;
+}
+
