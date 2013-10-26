f2b5dc8904775414267f4fd5d458be7bbe7fe039
Alexis Bernadet <alexis.bernadet@noos.fr>
2013-10-26 15:15:29 +0200

    Corrected typo in header.

--- src/elm_wrap.h
+++ src/elm_wrap.h
@@ -4,7 +4,7 @@
 #include "include.h"
 
 PREFIX inline Elm_Wrap_Type Elm_Wrap_Type_val(value v);
-PREFIX inline value Val_Elm_Wrap_Type_val(Elm_Wrap_Type t);
+PREFIX inline value Val_Elm_Wrap_Type(Elm_Wrap_Type t);
 
 PREFIX inline Elm_Icon_Type Elm_Icon_Type_val(value v);
 PREFIX inline value Val_Elm_Icon_Type(Elm_Icon_Type t);
