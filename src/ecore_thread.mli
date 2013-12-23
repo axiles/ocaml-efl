d015b8a9358338b76bd50b7cfc79a4e213ba43b4
Alexis Bernadet <alexis.bernadet@noos.fr>
2013-12-23 16:49:13 +0100

    Added ecore_thread_main_loop_begin/end.

---
+++ src/ecore_thread.mli
@@ -0,0 +1,8 @@
+val main_loop_begin_int : unit -> int
+
+val main_loop_begin : unit -> unit
+
+val main_loop_end_int : unit -> int
+
+val main_loop_end : unit -> unit
+
--- src/ecore_thread_wrap.c
+++ src/ecore_thread_wrap.c
@@ -1,2 +1,12 @@
-#include" include.h"
+#include "include.h"
+
+PREFIX value ml_ecore_thread_main_loop_begin(value v_unit)
+{
+        return Val_int(ecore_thread_main_loop_begin());
+}
+
+PREFIX value ml_ecore_thread_main_loop_end(value v_unit)
+{
+        return Val_int(ecore_thread_main_loop_end());
+}
 
