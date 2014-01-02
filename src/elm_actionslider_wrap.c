5a5f2345230230d729bda21fe2d3042d9dcc0da8
Alexis Bernadet <alexis.bernadet@noos.fr>
2014-01-02 16:38:45 +0100

    Added elm_action_slider_add.

--- src/elm_actionslider_wrap.c
+++ src/elm_actionslider_wrap.c
@@ -1,2 +1,9 @@
 #include "include.h"
 
+PREFIX value ml_elm_actionslider_add(value v_parent)
+{
+        Evas_Object* obj = elm_actionslider_add((Evas_Object*) v_parent);
+        if(obj == NULL) caml_failwith("elm_actionslider_add");
+        return (value) obj;
+}
+
