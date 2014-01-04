f4f41401d38182589cd66932ee74b9ac3e4bf471
Alexis Bernadet <alexis.bernadet@noos.fr>
2014-01-04 12:16:08 +0100

    Added Eina_List_Object_val.

--- src/eina_wrap.c
+++ src/eina_wrap.c
@@ -41,6 +41,17 @@ PREFIX inline value copy_Eina_List_Evas_Object(const Eina_List* list)
         CAMLreturn(v);
 }
 
+PREFIX inline Eina_List* Eina_List_Evas_Object_val(value v_list)
+{
+        Eina_List* list = NULL;
+        value v;
+        while(v != Val_int(0)) {
+                list = eina_list_append(list, (Evas_Object*) Field(v, 0));
+                v = Field(v, 1);
+        }
+        return list;
+}
+
 PREFIX inline value copy_Eina_List_Elm_Object_Item(const Eina_List* list)
 {
         CAMLparam0();
--- src/eina_wrap.h
+++ src/eina_wrap.h
@@ -9,6 +9,7 @@ PREFIX inline value Val_Eina_Bool(Eina_Bool b);
 PREFIX inline value copy_Eina_List_string(const Eina_List* list);
 
 PREFIX inline value copy_Eina_List_Evas_Object(const Eina_List* list);
+PREFIX inline Eina_List* Eina_List_Evas_Object_val(value v_list);
 
 PREFIX inline value copy_Eina_List_Elm_Object_Item(const Eina_List* list);
 
