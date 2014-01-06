d13699e688c0bf9df9d4add568dbf1aa8d1e44b0
Alexis Bernadet <alexis.bernadet@noos.fr>
2014-01-06 18:58:07 +0100

    Added elm_bubble_pos_set/get.

--- src/elm_bubble.mli
+++ src/elm_bubble.mli
@@ -2,3 +2,7 @@ type pos = [`invalid | `top_left | `top_right | `bottom_left | `bottom_right]
 
 val add : Evas.obj -> Evas.obj
 
+val pos_set : Evas.obj -> pos -> unit
+
+val pos_get : Evas.obj -> pos
+
