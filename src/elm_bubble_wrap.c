b2ae55d78c961eb2d086839f07aa1592853887e7
Alexis Bernadet <alexis.bernadet@noos.fr>
2014-01-06 18:40:11 +0100

    Added elm_bubble_add.

--- src/elm_bubble_wrap.c
+++ src/elm_bubble_wrap.c
@@ -1,2 +1,9 @@
 #include "include.h"
 
+PREFIX value ml_elm_bubble_add(Evas_Object* v_parent)
+{
+        Evas_Object* bubble = elm_bubble_add((Evas_Object*) v_parent);
+        if(bubble == NULL) caml_failwith("elm_bubble_add");
+        return (value) bubble;
+}
+
