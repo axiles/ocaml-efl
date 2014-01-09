77c6b75acd2f11e0c3ff1ca2ca7c1bb789cb054a
Alexis Bernadet <alexis.bernadet@noos.fr>
2014-01-09 12:54:00 +0100

    Added events in Elm_frame.

--- src/elm_frame.mli
+++ src/elm_frame.mli
@@ -1,3 +1,5 @@
+(** elm_frame_* *)
+
 val add : Evas.obj -> Evas.obj
 
 val autocollapse_set : Evas.obj -> bool -> unit
@@ -9,4 +11,9 @@ val collapse_set : Evas.obj -> bool -> unit
 val collapse_get : Evas.obj -> bool
 
 val collapse_go : Evas.obj -> bool -> unit
+
+module E : sig
+  type u = Evas.Event_type.u
+  val clicked : u
+end
 
