50ff4b39658dd6d11fc83ee2f4d62942d9613063
Alexis Bernadet <alexis.bernadet@noos.fr>
2014-01-13 14:01:29 +0100

    Added elm_panes_add.

---
+++ src/elm_panes.mli
@@ -0,0 +1,4 @@
+(** elm_panes_* *)
+
+val add : Evas.obj -> Evas.obj
+
--- src/elm_panes_wrap.c
+++ src/elm_panes_wrap.c
@@ -1,2 +1,9 @@
 #include "include.h"
 
+PREFIX value ml_elm_panes_add(value v_parent)
+{
+        Evas_Object* obj = elm_panes_add((Evas_Object*) v_parent);
+        if(obj == NULL) caml_failwith("elm_panes_add");
+        return (value) obj;
+}
+
