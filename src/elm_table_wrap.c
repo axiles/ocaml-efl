0028e20a0a501e7907ae2606b4d33fd719a98ec2
Alexis Bernadet <alexis.bernadet@noos.fr>
2014-01-14 07:48:34 +0100

    Added elm_table_add.

--- src/elm_table_wrap.c
+++ src/elm_table_wrap.c
@@ -1,2 +1,9 @@
 #include "include.h"
 
+PREFIX value ml_elm_table_add(value v_parent)
+{
+        Evas_Object* obj = elm_table_add((Evas_Object*) v_parent);
+        if(obj == NULL) caml_failwith("elm_table_add");
+        return (value) obj;
+}
+
