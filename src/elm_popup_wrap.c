58a818ff0caad856e06f7679289cd01eb4dc5059
Alexis Bernadet <alexis.bernadet@noos.fr>
2014-01-18 19:01:06 +0100

    Added elm_popup_add.

--- src/elm_popup_wrap.c
+++ src/elm_popup_wrap.c
@@ -1,2 +1,9 @@
 #include "include.h"
 
+PREFIX value ml_elm_popup_add(value v_parent)
+{
+        Evas_Object* popup = elm_popup_add((Evas_Object*) v_parent);
+        if(popup == NULL) caml_failwith("elm_popup_add");
+        return (value) popup;
+}
+
