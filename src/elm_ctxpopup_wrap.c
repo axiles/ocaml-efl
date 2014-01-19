cdd072ec65817d81a6787d0bd03dbfc447cc2d02
Alexis Bernadet <alexis.bernadet@noos.fr>
2014-01-19 11:19:03 +0100

    Added elm_cxtpopup_add.

--- src/elm_ctxpopup_wrap.c
+++ src/elm_ctxpopup_wrap.c
@@ -1,2 +1,9 @@
 #include "include.h"
 
+PREFIX value ml_elm_ctxpopup_add(value v_parent)
+{
+        Evas_Object* ctxpopup = elm_ctxpopup_add((Evas_Object*) v_parent);
+        if(ctxpopup == NULL) caml_failwith("elm_ctxpopup_add");
+        return (value) ctxpopup;
+}
+
