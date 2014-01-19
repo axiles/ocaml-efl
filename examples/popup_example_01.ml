607ff859f4c580a5cde3477c3698c1963af763c3
Alexis Bernadet <alexis.bernadet@noos.fr>
2014-01-19 07:40:08 +0100

    Added example popup_example_01.

---
+++ examples/popup_example_01.ml
@@ -0,0 +1,30 @@
+open Efl
+
+let block_clicked obj = Evas_object.hide obj
+
+let timeout obj = Evas_object.hide obj
+
+let () =
+  Elm.init ();
+  let win = Elm_win.util_standard_add "popup" "Popup" in
+  Elm_win.autodel_set win true;
+  Elm.policy_quit_set `last_window_closed;
+
+  let content = Elm_label.add win in
+  Elm_object.text_set content "<align=center>Content</align>";
+
+  let popup = Elm_popup.add win in
+  Elm_popup.timeout_set popup 3.;
+  Evas_object_smart.callback_add popup Elm_sig.timeout timeout;
+
+  Elm_object.content_set popup content;
+  Elm_object.part_text_set popup ~p:"title,text" "Title";
+  Evas_object.show popup;
+  Evas_object_smart.callback_add popup Elm_sig.block_clicked block_clicked;
+
+  Evas_object.show win;
+  Evas_object.resize win 480 800;
+
+  Elm.run ();
+  Elm.shutdown ()
+
