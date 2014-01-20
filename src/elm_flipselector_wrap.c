b553d9a48a03e0c7f05d19aa240ab00aab9004de
Alexis Bernadet <alexis.bernadet@noos.fr>
2014-01-20 16:59:49 +0100

    Added elm_flipselector_add.

--- src/elm_flipselector_wrap.c
+++ src/elm_flipselector_wrap.c
@@ -1,2 +1,10 @@
 #include "include.h"
 
+PREFIX value ml_elm_flipselector_add(value v_parent)
+{
+        Evas_Object* flipselector = elm_flipselector_add(
+                (Evas_Object*) v_parent);
+        if(flipselector == NULL) caml_failwith("elm_flipselector_add");
+        return (value) flipselector;
+}
+
