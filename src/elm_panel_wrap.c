04d3155153f579f4fb085505e5e1856c5e4c496c
Alexis Bernadet <alexis.bernadet@noos.fr>
2014-01-21 08:53:48 +0100

    Added elm_panel_add.

--- src/elm_panel_wrap.c
+++ src/elm_panel_wrap.c
@@ -1,2 +1,9 @@
 #include "include.h"
 
+PREFIX value ml_elm_panel_add(value v_parent)
+{
+        Evas_Object* panel = elm_panel_add((Evas_Object*) v_parent);
+        if(panel == NULL) caml_failwith("elm_panel_add");
+        return (value) panel;
+}
+
