689447758849a2aae77ab33cdbda6ccf64b643d7
Alexis Bernadet <alexis.bernadet@noos.fr>
2014-01-21 11:28:42 +0100

    Added elm_map_add.

--- src/elm_map_wrap.c
+++ src/elm_map_wrap.c
@@ -1,2 +1,9 @@
 #include "include.h"
 
+PREFIX value ml_elm_map_add(Evas_Object* v_parent)
+{
+        Evas_Object* map = elm_map_add((Evas_Object*) v_parent);
+        if(map == NULL) caml_failwith("elm_map_add");
+        return (value) map;
+}
+
