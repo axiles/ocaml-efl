c1d98fa0ac6fce8463669a3e8465390749116d58
Alexis Bernadet <alexis.bernadet@noos.fr>
2014-01-24 11:08:02 +0100

    Added elm_diskselector_add.

--- src/elm_diskselector_wrap.c
+++ src/elm_diskselector_wrap.c
@@ -1,2 +1,10 @@
 #include "include.h"
 
+PREFIX value ml_elm_diskselector_add(Evas_Object* v_parent)
+{
+        Evas_Object* diskselector = elm_diskselector_add(
+                (Evas_Object*) v_parent);
+        if(diskselector == NULL) caml_failwith("elm_diskselector_add");
+        return (value) diskselector;
+}
+
