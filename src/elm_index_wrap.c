e2c4bff4f0a495d37589d8877382b131bb4f1c8f
Alexis Bernadet <alexis.bernadet@noos.fr>
2014-01-27 19:10:44 +0100

    Added elm_index_add.

--- src/elm_index_wrap.c
+++ src/elm_index_wrap.c
@@ -1,2 +1,9 @@
 #include "include.h"
 
+PREFIX value ml_elm_index_add(value v_parent)
+{
+        Evas_Object* obj = elm_index_add((Evas_Object*) v_parent);
+        if(obj == NULL) caml_failwith("elm_index_add");
+        return (value) obj;
+}
+
