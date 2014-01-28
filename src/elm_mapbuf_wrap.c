84db2c0ba4f35676d1132cd3385d499986ea5e26
Alexis Bernadet <alexis.bernadet@noos.fr>
2014-01-28 22:55:58 +0100

    Added elm_mapbuf_add.

--- src/elm_mapbuf_wrap.c
+++ src/elm_mapbuf_wrap.c
@@ -1,2 +1,9 @@
 #include "include.h"
 
+PREFIX value ml_elm_mapbuf_add(Evas_Object* v_parent)
+{
+        Evas_Object* mapbuf = elm_mapbuf_add((Evas_Object*) v_parent);
+        if(mapbuf == NULL) caml_failwith("elm_mapbuf_add");
+        return (value) mapbuf;
+}
+
