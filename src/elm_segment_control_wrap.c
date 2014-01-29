4c8bc327bb83f7a8f3a5657adc8c54a6fc4ec0b4
Alexis Bernadet <alexis.bernadet@noos.fr>
2014-01-29 18:34:37 +0100

    Added elm_segment_control_add.

--- src/elm_segment_control_wrap.c
+++ src/elm_segment_control_wrap.c
@@ -1,2 +1,10 @@
 #include "include.h"
 
+PREFIX value ml_elm_segment_control_add(value v_parent)
+{
+        Evas_Object* segment_control = elm_segment_control_add(
+                (Evas_Object*) v_parent);
+        if(segment_control == NULL) caml_failwith("elm_segment_control_add");
+        return (value) segment_control;
+}
+
