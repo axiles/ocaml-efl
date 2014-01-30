4acae501b252d4d93effb948563b009d67e7691a
Alexis Bernadet <alexis.bernadet@noos.fr>
2014-01-30 13:23:45 +0100

    Added elm_slideshow_add.

--- src/elm_slideshow_wrap.c
+++ src/elm_slideshow_wrap.c
@@ -1,2 +1,9 @@
 #include "include.h"
 
+PREFIX value ml_elm_slideshow_add(value v_parent)
+{
+        Evas_Object* slideshow = elm_slideshow_add((Evas_Object*) v_parent);
+        if(slideshow == NULL) caml_failwith("elm_slideshow_add");
+        return (value) slideshow;
+}
+
