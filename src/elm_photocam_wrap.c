6fb7e447e3231060a6f6b4377892a4f175207432
Alexis Bernadet <alexis.bernadet@noos.fr>
2014-02-01 08:55:41 +0100

    Added elm_photocam_add.

--- src/elm_photocam_wrap.c
+++ src/elm_photocam_wrap.c
@@ -1,2 +1,9 @@
 #include "include.h"
 
+PREFIX value ml_elm_photocam_add(value v_parent)
+{
+        Evas_Object* photocam = elm_photocam_add((Evas_Object*) v_parent);
+        if(photocam == NULL) caml_failwith("elm_photocam_add");
+        return (value) photocam;
+}
+
