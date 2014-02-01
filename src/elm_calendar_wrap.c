2606e0bc5c6721a79b623b2183c180663eff6d85
Alexis Bernadet <alexis.bernadet@noos.fr>
2014-02-01 13:43:42 +0100

    Added elm_calendar_add.

--- src/elm_calendar_wrap.c
+++ src/elm_calendar_wrap.c
@@ -1,2 +1,9 @@
 #include "include.h"
 
+PREFIX value ml_elm_calendar_add(value v_parent)
+{
+        Evas_Object* calendar = elm_calendar_add((Evas_Object*) v_parent);
+        if(calendar == NULL) caml_failwith("elm_calendar_add");
+        return (value) calendar;
+}
+
