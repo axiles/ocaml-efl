13c439178298f84019ac5e1f5a82d23ffe022952
Alexis Bernadet <alexis.bernadet@noos.fr>
2014-02-02 19:19:30 +0100

    Added elm_colorselector_add.

--- src/elm_colorselector_wrap.c
+++ src/elm_colorselector_wrap.c
@@ -1,2 +1,10 @@
 #include "include.h"
 
+PREFIX value ml_elm_colorselector_add(value v_parent)
+{
+        Evas_Object* colorselector = elm_colorselector_add(
+                (Evas_Object*) v_parent);
+        if(colorselector == NULL) caml_failwith("elm_colorselector_add");
+        return (value) colorselector;
+}
+
