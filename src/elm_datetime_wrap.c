e0e9b9ad905c9f907cef5de4721edc473e047647
Alexis Bernadet <alexis.bernadet@noos.fr>
2014-02-04 06:56:52 +0100

    Added elm_datetime_add.

--- src/elm_datetime_wrap.c
+++ src/elm_datetime_wrap.c
@@ -1,2 +1,9 @@
 #include "include.h"
 
+PREFIX value ml_elm_datetime_add(value v_parent)
+{
+        Evas_Object* datetime = elm_datetime_add((Evas_Object*) v_parent);
+        if(datetime == NULL) caml_failwith("elm_datetime_add");
+        return (value) datetime;
+}
+
