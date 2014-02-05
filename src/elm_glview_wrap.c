e5e04de534d3a21528c7f55b3cad272fde61675e
Alexis Bernadet <alexis.bernadet@noos.fr>
2014-02-05 11:40:34 +0100

    Added elm_glview_add.

--- src/elm_glview_wrap.c
+++ src/elm_glview_wrap.c
@@ -1,2 +1,9 @@
 #include "include.h"
 
+PREFIX value ml_elm_glview_add(value v_parent)
+{
+        Evas_Object* glview = elm_glview_add((Evas_Object*) v_parent);
+        if(glview == NULL) caml_failwith("elm_glview_add");
+        return (value) glview;
+}
+
