ae92c9146a67881a00f0132171da943ff7dac25e
Alexis Bernadet <alexis.bernadet@noos.fr>
2014-02-05 08:21:23 +0100

    Added elm_dayselector_add.

---
+++ src/elm_dayselector.ml
@@ -0,0 +1,2 @@
+external add : Evas.obj -> Evas.obj = "ml_elm_dayselector_add"
+
--- src/elm_dayselector_wrap.c
+++ src/elm_dayselector_wrap.c
@@ -1,2 +1,9 @@
 #include "include.h"
 
+PREFIX value ml_elm_dayselector_add(value v_parent)
+{
+        Evas_Object* dayselector = elm_dayselector_add((Evas_Object*) v_parent);
+        if(dayselector == NULL) caml_failwith("elm_dayselector_add");
+        return (value) dayselector;
+}
+
