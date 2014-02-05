ae6ca22829a3b53776745a92f767d352eeeb4f96
Alexis Bernadet <alexis.bernadet@noos.fr>
2014-02-05 19:08:38 +0100

    Added elm_plug_add.

--- src/elm_plug.mli
+++ src/elm_plug.mli
@@ -1,2 +1,4 @@
 (** Plug *)
 
+val add : Evas.obj -> Evas.obj
+
--- src/elm_plug_wrap.c
+++ src/elm_plug_wrap.c
@@ -1,2 +1,9 @@
 #include "include.h"
 
+PREFIX value ml_elm_plug_add(value v_parent)
+{
+        Evas_Object* plug = elm_plug_add((Evas_Object*) v_parent);
+        if(plug == NULL) caml_failwith("elm_plug_add");
+        return (value) plug;
+}
+
