dbce4ad2f7a3e7f9627719c6d3dbf20f6721f6d2
Alexis Bernadet <alexis.bernadet@noos.fr>
2014-02-08 19:06:22 +0100

    Better GC with Elm_flipselector.

--- src/elm_flipselector_wrap.c
+++ src/elm_flipselector_wrap.c
@@ -33,19 +33,19 @@ PREFIX value ml_elm_flipselector_item_append(
                 data = NULL;
         } else {
                 func = ml_Evas_Smart_Cb;
-                data = (value*) caml_stat_alloc(sizeof(value));
-                *data = Field(v_func, 0);
-                caml_register_global_root(data);
+                data = ml_register_value(Field(v_func, 0));
         }
         Elm_Object_Item* item = elm_flipselector_item_append(
                 (Evas_Object*) v_obj, label, func, data);
         if(item == NULL) {
                 if(data != NULL) {
-                        caml_remove_global_root(data);
+                        caml_remove_generational_global_root(data);
                         free(data);
                 }
                 caml_failwith("elm_flipselector_item_append");
         }
+        if(data != NULL)
+                elm_object_item_del_cb_set(item, ml_Evas_Smart_Cb_on_del);
         return (value) item;
 }
 
@@ -62,19 +62,19 @@ PREFIX value ml_elm_flipselector_item_prepend(
                 data = NULL;
         } else {
                 func = ml_Evas_Smart_Cb;
-                data = (value*) caml_stat_alloc(sizeof(value));
-                *data = Field(v_func, 0);
-                caml_register_global_root(data);
+                data = ml_register_value(Field(v_func, 0));
         }
         Elm_Object_Item* item = elm_flipselector_item_prepend(
                 (Evas_Object*) v_obj, label, func, data);
         if(item == NULL) {
                 if(data != NULL) {
-                        caml_remove_global_root(data);
+                        caml_remove_generational_global_root(data);
                         free(data);
                 }
                 caml_failwith("elm_flipselector_item_prepend");
         }
+        if(data != NULL)
+                elm_object_item_del_cb_set(item, ml_Evas_Smart_Cb_on_del);
         return (value) item;
 }
 
