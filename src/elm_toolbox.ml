8cc5d56af99761fffd72dd59d055ac445556f879
Alexis Bernadet <alexis.bernadet@noos.fr>
2014-06-28 17:55:13 +0200

    Renamed the ~text argument by ~default_text in Elm_toolbox.input_line/text.

--- src/elm_toolbox.ml
+++ src/elm_toolbox.ml
@@ -98,8 +98,8 @@ let message_box ~title ?icon ?(ok = "Ok") ?parent msg cb =
 
   Evas_object.show win
 
-let input sl name ~title ?(ok = "Ok") ?(cancel = "Cancel") ?text ?parent msg
-    cb =
+let input sl name ~title ?(ok = "Ok") ?(cancel = "Cancel") ?default_text ?parent
+    msg cb =
   let win = add_dialog ?p:parent name in
   Elm_win.title_set win title;
   let quit x = Evas_object.del win; cb x in
@@ -115,7 +115,7 @@ let input sl name ~title ?(ok = "Ok") ?(cancel = "Cancel") ?text ?parent msg
   let entry = Elm_entry.add win in
   Evas_object.size_hint_set entry [`expand; `fill];
   Elm_box.pack_end box entry;
-  (match text with Some t -> Elm_entry.entry_set entry t | None -> ());
+  (match default_text with Some t -> Elm_entry.entry_set entry t | None -> ());
   Evas_object.show entry;
   Elm_entry.single_line_set entry sl;
   if sl then
